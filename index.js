46ddd21260ed57fcaaf208092e9f5217eae791e8
hypixelutils <55164697+hypixelutils@users.noreply.github.com>
2019-09-10 19:33:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ function boxEdited(num) {
 	input[2] = document.getElementById("EDB")
 	input[3] = document.getElementById("cost")
 	if(num === 0) {
-		input[1].text = parseInt(input[0]) / 160
-		input[2].text = parseInt(input[1]) / 160
-		input[3].text = parseInt(input[0]) * 8
+		input[1].value = parseInt(input[0].value) / 160
+		input[2].value = parseInt(input[1].value) / 160
+		input[3].value = parseInt(input[0].value) * 8
 	} else if (num === 1) {
-		input[0].text = parseInt(input[0]) * 160
-		input[2].text = parseInt(input[1]) / 160
-		input[3].text = parseInt(input[0]) * 8
+		input[0].value = parseInt(input[0].value) * 160
+		input[2].value = parseInt(input[1].value) / 160
+		input[3].value = parseInt(input[0].value) * 8
 	} else if (num === 2) {
-		input[0].text = parseInt(input[0]) * 160 * 160
-		input[1].text = parseInt(input[1]) / 160
-		input[3].text = parseInt(input[0]) * 8
+		input[0].value = parseInt(input[0].value) * 160 * 160
+		input[1].value = parseInt(input[1].value) / 160
+		input[3].value = parseInt(input[0].value) * 8
 	} else if (num === 3) {
-		input[0].text = parseInt(input[0]) / 8
-		input[1].text = parseInt(input[0]) / 160
-		input[2].text = parseInt(input[1]) / 160
+		input[0].value = parseInt(input[0].value) / 8
+		input[1].value = parseInt(input[0].value) / 160
+		input[2].value = parseInt(input[1].value) / 160
 	} 
 }
