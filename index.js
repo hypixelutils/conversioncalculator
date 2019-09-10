ce29cc20d51a7db14b3bc881248ef47f2edcfff5
hypixelutils <55164697+hypixelutils@users.noreply.github.com>
2019-09-10 19:45:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function boxEdited(num) {
 		input[3].value = parseInt(input[0].value) * 8
 	} else if (num === 2) {
 		input[0].value = parseInt(input[2].value) * 160 * 160
-		input[1].value = parseInt(input[0].value) / 160
-		input[3].value = parseInt(input[0].value) * 8
+		input[1].value = parseInt(input[2].value) * 160
+		input[3].value = parseInt(input[2].value) * 8 * 160 * 160
 	} else if (num === 3) {
 		input[0].value = parseInt(input[3].value) / 8
 		input[1].value = parseInt(input[0].value) / 160
